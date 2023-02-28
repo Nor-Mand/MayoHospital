2645e90895afd148e1c83cf761684abf3e45b2c3
Normand <normandlaredo@gmail.com>
2023-02-28 21:17:45 +0000

    [ADD] adding base structure on fron-page.php to show home page

--- footer.php
+++ footer.php
@@ -1,4 +1,24 @@
-
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <img src="" alt="footer image">
+                <p>Lorem, ipsum dolor.</p>
+            </div>
+            <div class="col-sm-4">
+                <img src="" alt="footer image">
+                <p>Lorem, ipsum dolor.</p>
+            </div>
+            <div class="col-sm-4">
+                <img src="" alt="footer image">
+                <p>Lorem, ipsum dolor.</p>
+            </div>
+        </div>
+        <div class="row">
+            menu footer......
+        </div>
+    </div>
+</footer>
 <?php wp_footer() ?>
 </body>
 </html>
---
+++ front-page.php
@@ -0,0 +1,258 @@
+<?php get_header() ?>
+<!-- Hero Section -->
+<section>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h1>Welcome to Mayo Hospital</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet sapiente velit sit consequuntur distinctio dolore quidem qui id ut explicabo.</p>
+                    <button class="btn btn-primary">Know More</button>
+                </div>
+                <div class="col-sm-8">
+                    <img src="" alt="Banner">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2">
+                <img src="" alt="icon">
+                <h5>Lorem ipsum dolor sit amet.</h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="" alt="icon">
+                <h5>Lorem ipsum dolor sit amet.</h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="" alt="icon">
+                <h5>Lorem ipsum dolor sit amet.</h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="" alt="icon">
+                <h5>Lorem ipsum dolor sit amet.</h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="" alt="icon">
+                <h5>Lorem ipsum dolor sit amet.</h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="" alt="icon">
+                <h5>Lorem ipsum dolor sit amet.</h5>
+            </div>
+        </div>
+    </main>
+</section>
+
+<!-- Services -->
+<section>
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">Our centres of excellence</h1>
+            <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, labore tempore soluta ratione repudiandae nesciunt error assumenda, sunt nam deleniti quo inventore aperiam nulla! Est dignissimos, facere nisi earum accusamus laboriosam eum mollitia veritatis consectetur, eveniet doloremque laborum eligendi sunt blanditiis voluptatem sequi ipsa iure exercitationem quisquam aperiam autem fuga quo ducimus excepturi! Earum suscipit labore laborum minima quos sunt!</p>
+        </div>
+        <div class="row">
+            <div class="col-sm-4">
+                <img src="" alt="Services">
+                <div>
+                    <h5>Lorem ipsum dolor sit amet consectetur.</h5>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <img src="" alt="Services">
+                <div>
+                    <h5>Lorem ipsum dolor sit amet consectetur.</h5>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <img src="" alt="Services">
+                <div>
+                    <h5>Lorem ipsum dolor sit amet consectetur.</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- About Section -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <h1>Welcome to Mayo Hospital</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita delectus maxime exercitationem, magnam accusantium vel sequi necessitatibus excepturi voluptatum impedit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cupiditate fugit laudantium doloremque ducimus, quidem id delectus repellendus esse nemo quae distinctio reprehenderit debitis velit obcaecati et harum tempora eaque.</p>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <img src="" alt="icon">
+                        <h5>Lorem ipsum dolor sit.</h5>
+                    </div>
+                    <div class="col-sm-6">
+                        <img src="" alt="icon">
+                        <h5>Lorem ipsum dolor sit.</h5>
+                    </div>
+                </div>
+                <button class="btn btn-primary">Know More</button>
+            </div>
+            <div class="col-sm-6">
+                <img src="" alt="Welcome Image">
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Contact Section -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="" alt="Image Contact">
+            </div>
+            <div class="col-sm-6">
+                <h1>Book Appointment</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis aspernatur molestias dolores quo, unde recusandae.</p>
+                [ Contact form 7 ]
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Testimonial Section -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <img src="" alt="testimonial imagen">
+                    </div>
+                    <div class="col-sm-10">
+                        <h5>Lorem, ipsum.</h5>
+                        <h6>Lorem, ipsum dolor.</h6>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eum atque dicta iusto molestiae itaque similique adipisci dolore repellendus, officia accusantium placeat, quam maiores veniam deserunt, eligendi laborum quas amet cupiditate cumque. Ad voluptatum commodi, possimus doloremque obcaecati quam est dolores saepe voluptates, soluta blanditiis! Ullam ipsum ea itaque iure.</p>
+            </div>
+            <div class="col-sm-6">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <img src="" alt="testimonial imagen">
+                    </div>
+                    <div class="col-sm-10">
+                        <h5>Lorem, ipsum.</h5>
+                        <h6>Lorem, ipsum dolor.</h6>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eum atque dicta iusto molestiae itaque similique adipisci dolore repellendus, officia accusantium placeat, quam maiores veniam deserunt, eligendi laborum quas amet cupiditate cumque. Ad voluptatum commodi, possimus doloremque obcaecati quam est dolores saepe voluptates, soluta blanditiis! Ullam ipsum ea itaque iure.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Banner Section -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <video src="">Video</video>
+            </div>
+            <div class="col-sm-6">
+                <h1>Contact Informaction</h1>
+                <p>addres: Lorem ipsum dolor sit amet.</p>
+                <p>addres: Lorem ipsum dolor sit amet.</p>
+
+                <p>phone: Lorem ipsum dolor sit.</p>
+                <p>phone: Lorem ipsum dolor sit.</p>
+
+                <p>email: Lorem, ipsum dolor.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- News Section -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h2>Mayo news</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus nisi dolore enim hic, deleniti non explicabo animi tenetur corporis, accusantium voluptas dolores esse aspernatur placeat aliquid ad quod sapiente ipsum!</p>
+                <button class="btn btn-primary">Read all news</button>
+            </div>
+            <div class="col-sm-8">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h6>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, vitae.</h6>
+                        <div>
+                            <img src="" alt="icon">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, illum!</p>
+                        </div>
+                        <button class="btn btn-warning">Read More</button>
+                    </div>
+                    <div class="col-sm-4">
+                        <h6>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, vitae.</h6>
+                        <div>
+                            <img src="" alt="icon">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, illum!</p>
+                        </div>
+                        <button class="btn btn-warning">Read More</button>
+                    </div>
+                    <div class="col-sm-4">
+                        <h6>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, vitae.</h6>
+                        <div>
+                            <img src="" alt="icon">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, illum!</p>
+                        </div>
+                        <button class="btn btn-warning">Read More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Blog Section -->
+<section>
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">Our blog</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit perferendis, porro, accusantium eligendi sit ratione, soluta laudantium consectetur provident veritatis suscipit quas earum sapiente necessitatibus eos ipsam nostrum officiis nesciunt dolorem expedita quibusdam itaque. Harum error odit quasi nemo perspiciatis!</p>
+            <div class="row">
+                <div class="col-sm-4">
+                    <img src="" alt="blog image">
+                    <h5>Lorem ipsum dolor sit amet consectetur.</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At accusamus nostrum eligendi dignissimos sequi?</p>
+                    <button class="btn btn-outline-dark">Read More</button>
+                </div>
+                <div class="col-sm-4">
+                    <img src="" alt="blog image">
+                    <h5>Lorem ipsum dolor sit amet consectetur.</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At accusamus nostrum eligendi dignissimos sequi?</p>
+                    <button class="btn btn-outline-dark">Read More</button>
+                </div>
+                <div class="col-sm-4">
+                    <img src="" alt="blog image">
+                    <h5>Lorem ipsum dolor sit amet consectetur.</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At accusamus nostrum eligendi dignissimos sequi?</p>
+                    <button class="btn btn-outline-dark">Read More</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Appointment Section -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <img src="" alt="Appointment image">
+            </div>
+            <div class="col-sm-8">
+                <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi, perferendis?</h3>
+                <h5>Lorem ipsum dolor sit amet.</h5>
+                <button class="btn btn-primary">Make Appointment</button>
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer() ?>
